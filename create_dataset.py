94de7beb1c9730c9fa5c432826986971bbe1ad74
NataSonyaTasya <117571148+NataSonyaTasya@users.noreply.github.com>
2023-06-13 21:51:26 +0300

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -8,5 +8,5 @@ test['Sex'] = test['Sex'].apply(lambda x: 0 if 'male' else 1)
 train['Age'] = train['Age'].fillna(train.Age.mean())
 test['Age'] = test['Age'].fillna(train.Age.mean())
 # запишем созданные датасеты во внешние csv файлы
-train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Survived']].to_csv('data_train.csv', index=False)
-test[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].to_csv('data_test.csv', index=False)
+train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Survived']].to_csv('/var/lib/jenkins/workspace/AMO_2/AMO_2/home/ubuntu/project/AMO_2/data_train.csv', index=False)
+test[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].to_csv('/var/lib/jenkins/workspace/AMO_2/AMO_2/home/ubuntu/project/AMO_2/data_test.csv', index=False)
